373aeb9230f4976d744d40d0dc1f3288d625a33d
Menno Kramer <mennockramer@gmail.com>
2022-08-19 17:50:59 +0100

    Moved to using LibreHardwareMonitor

--- MQTTHardwareInfo.py
+++ MQTTHardwareInfo.py
@@ -5,33 +5,59 @@ import datetime
 import clr
 import os
 
-
-
 def on_message(client, userdata, message):
     print("Message: " ,str(message.payload.decode("utf-8")))
     print("Topic  : ",message.topic)
     print("QoS    : ",message.qos)
     print("Retain : ",message.retain)
 
-
-
-clr.AddReference(os.path.dirname(__file__)+"\OpenHardwareMonitorLib.dll")
-from OpenHardwareMonitor import Hardware
+clr.AddReference(os.path.dirname(__file__)+"\LibreHardwareMonitorLib.dll")
+from LibreHardwareMonitor import Hardware
 computer = Hardware.Computer()
-computer.MainboardEnabled=True
-computer.CPUEnabled=True
-computer.RAMEnabled=True
-computer.GPUEnabled=True
-computer.HDDEnabled=True
+computer.IsMotherboardEnabled=True
+computer.IsCpuEnabled=True
+computer.IsMemoryEnabled=True
+computer.IsGpuEnabled=True
+computer.IsStorageEnabled=True
+computer.IsNetworkEnabled=True
 computer.Open()
 
+#yes these are hardcoded 
 hardwareTypes = [ 
-  'Mainboard', 'SuperIO', 'CPU', 'RAM', 'GpuNvidia', 'GpuAti', 'TBalancer', 'Heatmaster', 'HDD' 
-]
+  "Motherboard",
+  "SuperIO",
+  "Cpu",
+  "Memory",
+  "GpuNvidia",
+  "GpuAmd",
+  "GpuIntel",
+  "Storage",
+  "Network",
+  "Cooler",
+  "EmbeddedController",
+  "Psu",
+  "Battery"
+  ]
 
 sensorTypes = [
- 'Voltage', 'Clock', 'Temperature', 'Load', 'Fan', 'Flow', 'Control', 'Level', 'Factor', 'Power', 'Data', 'SmallData'
-]
+  "Voltage", # V
+  "Current", # A
+  "Power", # W
+  "Clock", # MHz
+  "Temperature", # °C
+  "Load", # %
+  "Frequency", # Hz
+  "Fan", # RPM
+  "Flow", # L/h
+  "Control", # %
+  "Level", # %
+  "Factor", # 1
+  "Data", # GB = 2^30 Bytes
+  "SmallData", # MB = 2^20 Bytes
+  "Throughput", # B/s
+  "TimeSpan", # Seconds 
+  "Energy" # milliwatt-hour (mWh)
+  ]
 
 client = paho.mqtt.client.Client(socket.gethostname()+"-MQTTHardwareInfo")
 client.username_pw_set(username="user", password="passwd")
@@ -47,8 +73,9 @@ while True:
   firstHardware = True
   for h in computer.Hardware :
     h.Update()
-    print(h.Name)
-
+    #print(h.Name)
+    #print(h.HardwareType)
+    #print(hardwareTypes[h.HardwareType])
     if firstHardware == True or payload == "{\"MQTTHardwareInfo\":[" or h.Sensors == [] :
         firstHardware = False
     else :
@@ -64,20 +91,24 @@ while True:
         payload = payload + ","
 
       
+      #print("Name:  "+ s.Name)
+      #print("Type:  "+ str(s.SensorType))
+      #print("Type:  "+ sensorTypes[s.SensorType])
+      #print("Value: "+ str(s.Value))
       payload = payload + "{\"Name\":\""+s.Name+"\","+ "\"SensorType\":\""+sensorTypes[s.SensorType]+"\", \"Value\":\""+str(s.Value)+"\"}"
       
     firstSubHardware =True
     for sub in h.SubHardware :
       sub.Update()
       #print(sub.Name)
-
-      
-
-      
+         
 
       firstSubSensor =True
       for ss in sub.Sensors :
-        #print(ss.Name + " " + sensorTypes[ss.SensorType]+ " " +str(ss.Value) )
+        #print("Name:  "+ ss.Name)
+        #print("Type:  "+ str(ss.SensorType))
+        #print("Type:  "+ sensorTypes[ss.SensorType])
+        #print("Value: "+ str(ss.Value))
         if firstSubSensor == True :
           firstSubSensor = False
         else :
